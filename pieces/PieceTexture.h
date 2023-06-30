b7a987502eaa5b40d510686197b1cb930ef0fa81
Richard_Chu <liangforcoding@gmail.com>
2023-06-30 19:27:02 +0800

    add PieceTexture.cpp and PieceTexture.h

--- pieces/PieceTexture.h
+++ pieces/PieceTexture.h
@@ -5,9 +5,24 @@
 #ifndef CHESS_PIECETEXTURE_H
 #define CHESS_PIECETEXTURE_H
 
+#include <SFML/Graphics.hpp>
 
 class PieceTexture {
+public:
+    static sf::Texture whiteKing;
+    static sf::Texture blackKing;
+    static sf::Texture whiteQueen;
+    static sf::Texture blackQueen;
+    static sf::Texture whiteBishop;
+    static sf::Texture blackBishop;
+    static sf::Texture whiteKnight;
+    static sf::Texture blackKnight;
+    static sf::Texture whiteRook;
+    static sf::Texture blackRook;
+    static sf::Texture whitePawn;
+    static sf::Texture blackPawn;
 
+    static sf::Texture loadTexture(const char *name);
 };
 
 
