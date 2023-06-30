a7154ce1fd8aae4ce21473be85daf7e9aa8f78ed
Richard_Chu <liangforcoding@gmail.com>
2023-06-30 18:55:02 +0800

    add PieceTexture.cpp and PieceTexture.h

---
+++ pieces/PieceTexture.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 褚翊喨 on 2023/6/30.
+//
+
+#include "PieceTexture.h"
---
+++ pieces/PieceTexture.h
@@ -0,0 +1,14 @@
+//
+// Created by 褚翊喨 on 2023/6/30.
+//
+
+#ifndef CHESS_PIECETEXTURE_H
+#define CHESS_PIECETEXTURE_H
+
+
+class PieceTexture {
+
+};
+
+
+#endif //CHESS_PIECETEXTURE_H
