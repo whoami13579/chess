4d8d3f85f218e33d818ded4a8a86aa17b221c626
whoami13579 <liangforcoding@gmail.com>
2024-06-24 01:20:27 +0800

    update PieceTexture.cpp

--- PieceTexture.cpp
+++ PieceTexture.cpp
@@ -11,15 +11,15 @@ sf::Texture PieceTexture::loadTexture(const char *name) {
     return tmp;
 }
 
-sf::Texture PieceTexture::whiteKing = PieceTexture::loadTexture("/Users/mba/myCode/chess/Textures/w_king.png");
-sf::Texture PieceTexture::blackKing = PieceTexture::loadTexture("/Users/mba/myCode/chess/Textures/b_king.png");
-sf::Texture PieceTexture::whiteQueen = PieceTexture::loadTexture("/Users/mba/myCode/chess/Textures/w_queen.png");
-sf::Texture PieceTexture::blackQueen = PieceTexture::loadTexture("/Users/mba/myCode/chess/Textures/b_queen.png");
-sf::Texture PieceTexture::whiteBishop = PieceTexture::loadTexture("/Users/mba/myCode/chess/Textures/w_bishop.png");
-sf::Texture PieceTexture::blackBishop = PieceTexture::loadTexture("/Users/mba/myCode/chess/Textures/b_bishop.png");
-sf::Texture PieceTexture::whiteKnight = PieceTexture::loadTexture("/Users/mba/myCode/chess/Textures/w_knight.png");
-sf::Texture PieceTexture::blackKnight = PieceTexture::loadTexture("/Users/mba/myCode/chess/Textures/b_knight.png");
-sf::Texture PieceTexture::whiteRook = PieceTexture::loadTexture("/Users/mba/myCode/chess/Textures/w_rook.png");
-sf::Texture PieceTexture::blackRook = PieceTexture::loadTexture("/Users/mba/myCode/chess/Textures/b_rook.png");
-sf::Texture PieceTexture::whitePawn = PieceTexture::loadTexture("/Users/mba/myCode/chess/Textures/w_pawn.png");
-sf::Texture PieceTexture::blackPawn = PieceTexture::loadTexture("/Users/mba/myCode/chess/Textures/b_pawn.png");
+sf::Texture PieceTexture::whiteKing = PieceTexture::loadTexture("/Users/mba/Code/chess/Textures/w_king.png");
+sf::Texture PieceTexture::blackKing = PieceTexture::loadTexture("/Users/mba/Code/chess/Textures/b_king.png");
+sf::Texture PieceTexture::whiteQueen = PieceTexture::loadTexture("/Users/mba/Code/chess/Textures/w_queen.png");
+sf::Texture PieceTexture::blackQueen = PieceTexture::loadTexture("/Users/mba/Code/chess/Textures/b_queen.png");
+sf::Texture PieceTexture::whiteBishop = PieceTexture::loadTexture("/Users/mba/Code/chess/Textures/w_bishop.png");
+sf::Texture PieceTexture::blackBishop = PieceTexture::loadTexture("/Users/mba/Code/chess/Textures/b_bishop.png");
+sf::Texture PieceTexture::whiteKnight = PieceTexture::loadTexture("/Users/mba/Code/chess/Textures/w_knight.png");
+sf::Texture PieceTexture::blackKnight = PieceTexture::loadTexture("/Users/mba/Code/chess/Textures/b_knight.png");
+sf::Texture PieceTexture::whiteRook = PieceTexture::loadTexture("/Users/mba/Code/chess/Textures/w_rook.png");
+sf::Texture PieceTexture::blackRook = PieceTexture::loadTexture("/Users/mba/Code/chess/Textures/b_rook.png");
+sf::Texture PieceTexture::whitePawn = PieceTexture::loadTexture("/Users/mba/Code/chess/Textures/w_pawn.png");
+sf::Texture PieceTexture::blackPawn = PieceTexture::loadTexture("/Users/mba/Code/chess/Textures/b_pawn.png");
